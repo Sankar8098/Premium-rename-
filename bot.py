5bdfb10f37ad7ca6ff1b4b520e6e720e5ed2d010
Hansaka Anuhas <120485409+anuhas1233@users.noreply.github.com>
2023-01-03 15:54:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 import logging 
 from pyrogram import Client, __version__
 from user import User, LOGGER
+from aiohttp import web
+from plugins import web_server
 # import pyromod.listen
 
 logging.getLogger().setLevel(logging.INFO)
@@ -11,6 +13,8 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
+
+PORT = os.environ.get("PORT", "8080")
 
 class Bot(Client):  
     USER: User = None
@@ -36,6 +40,10 @@ class Bot(Client):
             f"@{usr_bot_me.username}  started! "
         )
         self.USER, self.USER_ID = await User().start()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
